3d7e6b6d33eca9b14e81b27d0885f875dc74c72e
Shiv0567 <shivamcb96@gmail.com>
2025-06-16 14:34:31 +0530

    Add files via upload

---
+++ Skip_multiples.java
@@ -0,0 +1,16 @@
+package Conditional_Statements;
+
+public class Skip_multiples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0 ; i<= 100 ; i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println("The Final Digits are " +i);
+			}
+			
+		}
+
+	}
+
+}
