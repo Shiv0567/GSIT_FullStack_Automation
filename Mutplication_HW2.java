006795c24dd68cf378e1c12434e9dcf5a214c051
Shiv0567 <shivamcb96@gmail.com>
2025-06-16 14:40:18 +0530

    Add files via upload

---
+++ Mutplication_HW2.java
@@ -0,0 +1,17 @@
+package Loops_Java;
+
+public class Mutplication_HW2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j; 
+	    for(i = 1 ; i<=5; i++) {
+	    	for(j = 1 ; j<=10; j++) {
+	    		int result = i*j;
+	    		System.out.println(i +"x" + j + "=" + result);
+	    	}
+	    }
+
+	}
+
+}
